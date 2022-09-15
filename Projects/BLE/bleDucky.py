2c936902f4d24f5fbdabe30d8f2e6e9cda8bb7c4
aarnas <arnas.andriuskevicius@gmail.com>
2022-09-15 08:34:35 +0300

    Bluetooth ducky example

---
+++ Projects/BLE/bleDucky.py
@@ -0,0 +1,95 @@
+import time
+import usb_hid
+from adafruit_hid.mouse import Mouse
+import board
+import busio
+import digitalio
+
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS as KeyboardLayout
+from adafruit_hid.keycode import Keycode
+
+mouse = Mouse(usb_hid.devices)
+kbd = Keyboard(usb_hid.devices)
+layout = KeyboardLayout(kbd)
+
+led = digitalio.DigitalInOut(board.GP25)
+led.direction = digitalio.Direction.OUTPUT
+led.value = False
+uart = busio.UART(board.GP0, board.GP1, baudrate=9600)
+
+duckyCommands = {
+    'WINDOWS': Keycode.WINDOWS, 'GUI': Keycode.GUI,
+    'APP': Keycode.APPLICATION, 'MENU': Keycode.APPLICATION, 'SHIFT': Keycode.SHIFT,
+    'ALT': Keycode.ALT, 'CONTROL': Keycode.CONTROL, 'CTRL': Keycode.CONTROL,
+    'DOWNARROW': Keycode.DOWN_ARROW, 'DOWN': Keycode.DOWN_ARROW, 'LEFTARROW': Keycode.LEFT_ARROW,
+    'LEFT': Keycode.LEFT_ARROW, 'RIGHTARROW': Keycode.RIGHT_ARROW, 'RIGHT': Keycode.RIGHT_ARROW,
+    'UPARROW': Keycode.UP_ARROW, 'UP': Keycode.UP_ARROW, 'BREAK': Keycode.PAUSE,
+    'PAUSE': Keycode.PAUSE, 'CAPSLOCK': Keycode.CAPS_LOCK, 'DELETE': Keycode.DELETE,
+    'END': Keycode.END, 'ESC': Keycode.ESCAPE, 'ESCAPE': Keycode.ESCAPE, 'HOME': Keycode.HOME,
+    'INSERT': Keycode.INSERT, 'NUMLOCK': Keycode.KEYPAD_NUMLOCK, 'PAGEUP': Keycode.PAGE_UP,
+    'PAGEDOWN': Keycode.PAGE_DOWN, 'PRINTSCREEN': Keycode.PRINT_SCREEN, 'ENTER': Keycode.ENTER,
+    'SCROLLLOCK': Keycode.SCROLL_LOCK, 'SPACE': Keycode.SPACE, 'TAB': Keycode.TAB,
+    'A': Keycode.A, 'B': Keycode.B, 'C': Keycode.C, 'D': Keycode.D, 'E': Keycode.E,
+    'F': Keycode.F, 'G': Keycode.G, 'H': Keycode.H, 'I': Keycode.I, 'J': Keycode.J,
+    'K': Keycode.K, 'L': Keycode.L, 'M': Keycode.M, 'N': Keycode.N, 'O': Keycode.O,
+    'P': Keycode.P, 'Q': Keycode.Q, 'R': Keycode.R, 'S': Keycode.S, 'T': Keycode.T,
+    'U': Keycode.U, 'V': Keycode.V, 'W': Keycode.W, 'X': Keycode.X, 'Y': Keycode.Y,
+    'Z': Keycode.Z, 'F1': Keycode.F1, 'F2': Keycode.F2, 'F3': Keycode.F3,
+    'F4': Keycode.F4, 'F5': Keycode.F5, 'F6': Keycode.F6, 'F7': Keycode.F7,
+    'F8': Keycode.F8, 'F9': Keycode.F9, 'F10': Keycode.F10, 'F11': Keycode.F11,
+    'F12': Keycode.F12,
+}
+
+def convertLine(line):
+    newline = []
+    # loop on each key - the filter removes empty values
+    for key in filter(None, line.split(" ")):
+        key = key.upper()
+        # find the keycode for the command in the list
+        command_keycode = duckyCommands.get(key, None)
+        if command_keycode is not None:
+            # if it exists in the list, use it
+            newline.append(command_keycode)
+        elif hasattr(Keycode, key):
+            # if it's in the Keycode module, use it (allows any valid keycode)
+            newline.append(getattr(Keycode, key))
+        else:
+            # if it's not a known key name, show the error for diagnosis
+            print(f"Unknown key: <{key}>")
+    return newline
+
+def runScriptLine(line):
+    for k in line:
+        kbd.press(k)
+    kbd.release_all()
+
+def sendString(line):
+    layout.write(line)
+
+def parseLine(line):
+    if(line[0:3] == "REM"):
+        pass
+    elif(line[0:6] == "STRING"):
+        sendString(line[7:])
+    elif(line[0:6] == "MOUSEY"):
+        mouse.move(y=int(line[7:]))
+    elif(line[0:6] == "MOUSEX"):
+        mouse.move(x=int(line[7:]))
+    else:
+        newScriptLine = convertLine(line)
+        runScriptLine(newScriptLine)
+
+f = open('text.txt',"r",encoding='utf-8')
+
+while True:
+    data = uart.read(32)
+    if data is not None:
+        data_string = ''.join([chr(b) for b in data])
+        data_string_length = len(data_string)
+        if data_string_length > 2:
+            parseLine(data_string[0:data_string_length-2])
+            data_string_length = 0
+            text = f.read()
+            uart.write(b'' + text.encode() + b'')
+
